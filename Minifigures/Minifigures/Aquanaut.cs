294ead5cd15ccffa55e3de77a9d6e7e73d64ba1c
Robert Bandstra <rob.bandstra@gmail.com>
2021-01-21 21:09:49 -0600

    added aquanaut class

---
+++ Minifigures/Minifigures/Aquanaut.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Minifigures.Minifigures
+{
+    class Aquanaut
+    {
+    
+        //Properties
+        public string Hat { get; set; }
+        public string Head { get; set; }
+        public string Torso { get; set; }
+        public string Legs { get; set; }
+        public string Accessories { get; set; }
+
+        //Methods
+        public void Jump()
+        {
+            Console.WriteLine("Aquanaut jumps");
+        }
+        public void DoubleJump()
+        {
+            Console.WriteLine("Aquanaut jumps again in the air somehow");
+        }
+        public void Attack(bool hitBox)
+        {
+            if (hitBox)
+            {
+                Console.WriteLine("Aquanaut's punch lands. Whammo!");
+            }
+            else
+            {
+                Console.WriteLine("Aquanaut punches the air.");
+            }
+        }
+        public void SpecialAttack(bool coolDown)
+        {
+            if (coolDown)
+            {
+                Console.WriteLine("That attack isn't ready yet.");
+            }
+            else
+            {
+                Console.WriteLine("Aquanaut fires their harpoon!");
+            }
+        }
+        public void Look()
+        {
+            Console.WriteLine("Aquanaut looks around");
+        }
+
+        public void Move()
+        {
+            if (Accessories == "Jetpack")
+            {
+                Console.WriteLine("Aquanaut flies through the air.");
+            }
+            else
+            {
+                Console.WriteLine("Aquanaut runs forward.");
+            }
+        }
+    }
+}
---
+++ Minifigures/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Minifigures.Minifigures;
+
+namespace Minifigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var aquanaut1 = new Aquanaut
+            {
+                Hat = "Diving Helmet",
+                Head = "Goggles",
+                Torso = "Underwater Armor",
+                Legs = "Underwater Armor",
+                Accessories = "Jetpack"
+            };
+            aquanaut1.SpecialAttack(true);
+        }
+        
+    }
+}
